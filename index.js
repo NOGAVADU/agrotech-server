e6e89fd1183a80c912a50b093810b31f3f506ad1
Даниил Мосолов <dkuets@gmail.com>
2024-03-29 18:06:21 +0300

    remove last changes

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const path = require ('path')
-const https = require('https')
-const fs = require('fs')
-const morgan = require('morgan')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
@@ -14,24 +10,16 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
-app.use(morgan("dev"));
 app.use(express.json())
 app.use('/api', router)
 
 app.use(errorHandler)
 
-const options = {
-    key: fs.readFileSync(path.join(__dirname, "localhost-key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "localhost.pem")),
-};
-
-const server = https.createServer(options, app);
-
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        server.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
+        app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
     } catch (e) {
         console.log(e)
     }
