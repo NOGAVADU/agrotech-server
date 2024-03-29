490e47a928bf3c043e45ff892a12bce4b1f08d42
Даниил Мосолов <dkuets@gmail.com>
2024-03-30 00:02:53 +0300

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/ErrorHandlingMiddleware')
 
-const PORT = process.env.PORT || 443
+const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
