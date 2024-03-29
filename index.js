715a7dc9a577e6d96b5d2773bea9b43763695e27
Даниил Мосолов <dkuets@gmail.com>
2024-03-30 00:01:03 +0300

    s

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ const cors = require('cors')
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/ErrorHandlingMiddleware')
 
-const https = require("https");  // для организации https
-const fs = require("fs");
-
-const PORT = process.env.PORT || 5000
-
-httpsOptions = {
-    key: fs.readFileSync("key.pem"), // путь к ключу
-    cert: fs.readFileSync("cert.pem") // путь к сертификату
-}
+const PORT = process.env.PORT || 443
 
 const app = express()
 app.use(cors())
@@ -27,16 +19,12 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        https.createServer({
-            key: fs.readFileSync(httpsOptions.key),
-            cert: fs.readFileSync(httpsOptions.cert),
-        },).listen(PORT, () => {
+        app.listen(PORT, () => {
             console.log(`Server started on port ${PORT}`)
         })
     } catch (e) {
         console.log(e)
     }
 }
-
 
 start()
