cd5a36befd5c2543bc1f0b9e3d7a0e9e97b651b9
Даниил Мосолов <dkuets@gmail.com>
2024-03-29 17:55:58 +0300

    add https sequire

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const path = require ('path')
+const https = require('https')
+const fs = require('fs')
+const morgan = require('morgan')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
@@ -10,16 +14,24 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
+app.use(morgan("dev"));
 app.use(express.json())
 app.use('/api', router)
 
 app.use(errorHandler)
 
+const options = {
+    key: fs.readFileSync(path.join(__dirname, "localhost-key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "localhost.pem")),
+};
+
+const server = https.createServer(options, app);
+
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
+        server.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
     } catch (e) {
         console.log(e)
     }
