767ef035a84b8c100ac601b0a19d63918159f53f
Даниил Мосолов <dkuets@gmail.com>
2024-03-31 23:14:23 +0300

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(errorHandler)
 const start = async () => {
     try {
         await sequelize.authenticate()
-        await sequelize.sync({alter: true})
+        await sequelize.sync()
         app.listen(PORT, () => {
             console.log(`Server started on port ${PORT}`)
         })
